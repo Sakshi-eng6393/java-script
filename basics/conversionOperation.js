4a403d4eea3584d4cd60265a346e8a154fe21b74
Sakshi-eng6393 <guptasakshi6521@gmail.com>
2026-06-28 20:48:44 +0000

    done

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -21,5 +21,36 @@ let booleanIsLoggedIn= Boolean(isLoggedIn)
 let someNumber=33
 
 let stringNumber=String(someNumber)
-console.log(typeof stringNumber);
+//console.log(typeof stringNumber);
+
+
+
+//****************operations************** */
+
+let value=3
+let negValue=-value
+//console.log(negValue);
+
+let str1="hello"
+let str2=" sakshi"
+
+let str3=str1 +str2
+//console.log(str3);
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gameCounter=100
+++gameCounter
+console.log(gameCounter);
+
+//link to study
+
+//https://tc39.es/ecma262/multipage/
+// abstract-operations.html#sec-type-conversion
+
+
+
+
 
