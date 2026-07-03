60a9b3f7be0d08b5930b7da4940568da94e1561d
Sakshi-eng6393 <guptasakshi6521@gmail.com>
2026-07-03 15:26:51 +0000

    datatypes

---
+++ basics/comparison.js
@@ -0,0 +1,16 @@
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null >0);
+// console.log(null ==0);
+// console.log(null >=0);//the reasons is that an equality check == and comparison > <>=<= work differently.
+//Comparison convert null to a number ,traeting it as 0.
+//That's why (3) null>=0 is true and(1) null>0 is false.
+
+
+console.log(undefined == 0);
+
+//===
+
+console.log("2" === 2);
+
---
+++ basics/datatype.js
@@ -0,0 +1,27 @@
+// #Primitive data type
+
+//7 types:String,Number,boolean,null,undefined,Symbol,BigInt
+
+// const score=100
+// const scoreValue=100.3
+// const isLoggedIn=false
+// const outsideTemp=null
+// let userEmail
+
+const  id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id === anotherId);
+
+// Referrence types (Non primitive)
+const heros=["sakshi","shrishti","shital"]
+let myObj={
+    name:"sakshi",
+    age:22
+}
+
+const myFunction=function(){
+    console.log("hello world");
+    
+}
+//Array,objects,functions,
